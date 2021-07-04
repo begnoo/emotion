ca3f9b30499cf3e44cc339f733bb77ea680f831b
Zoran <zoranjankov99@gmail.com>
2021-07-04 19:00:40 +0200

    feat: added cnn models and modules for training and prediction

---
+++ cnn/predict.py
@@ -0,0 +1,32 @@
+import numpy as np
+from cv2.cv2 import CascadeClassifier, imread, IMREAD_GRAYSCALE
+
+FER_TEST_PATH = "../fer2013/test"
+CK_TEST_PATH = "../ck+extracted/test"
+
+fer_labels = ['anger', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+ck_labels = ["anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"]
+
+
+def load(dataset):
+    return models.load_model('model_with_' + dataset + '.h5')
+
+def predict(image, dataset):
+    model = load(dataset)
+    reshaped_image = np.array(image, dtype=np.float32)
+    reshaped_image = np.reshape(reshaped_image, (-1, 48, 48, 1))
+    prediction_classes = model.predict(reshaped_image)
+    prediction = prediction_classes.argmax()
+    return ck_labels[prediction] if dataset == 'ck' else fer_labels[prediction]
+
+
+def evaluate(dataset):
+    path = CK_TRAINING_PATH if dataset == "ck" else FER_TRAINING_PATH
+    test_data = ImageDataGenerator().flow_from_directory(directory=path,
+                                                     target_size=(48, 48),
+                                                     shuffle=True, color_mode='grayscale')
+    model = load(dataset)
+    model.evaluate(test_data)
+
+if __name__ == '__main__':
+    evaluate("ck")
